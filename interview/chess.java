2649081f19de0b902c1dcf04bf01e96fab36a151
CreativeNob <73249926+CreativeNob@users.noreply.github.com>
2020-11-13 13:13:56 +0500

    Update chess.java

--- interview/chess.java
+++ interview/chess.java
@@ -6,36 +6,75 @@
  */
  /* package codechef; // don't place package name! */
 
+/* package codechef; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 public class Main {
+    public static ArrayList<int[]> placeQueens(int gridSize) {
+        ArrayList<int[]> result = new ArrayList<int[]>();
+        placeQueens(0, new int[gridSize], result, gridSize);
+        return result;
+    }
 
-	public static int countPaths(int[][] target, int n, int row, int col){
+    private static void placeQueens(int row, int[] solution, ArrayList<int[]> solutions, int gridSize) {
+        if (row == gridSize) {
+            solutions.add(solution.clone());
+            return;
+        }
+        for (int col = 0; col < gridSize; ++col) {
+            if (canPlace(row, col, solution)) {
+                solution[row] = col;
+                placeQueens(row + 1, solution, solutions, gridSize);
+            }
+        }
+    }
 
-		if (n == 1)
-		return 2;
+    /*
+     * FOLLOW UP
+     * What if you only need to count the number of solutions?
+     */
+    public static int placeQueensNum(int gridSize) {
+        return placeQueensNum(0, new int[gridSize], gridSize);
+    }
 
-		int innerCount = 0;
+    private static int placeQueensNum(int row, int[] solution, int gridSize) {
+        if (row == gridSize) {
+            return 1;
+        }
+        int num = 0;
+        for (int col = 0; col < gridSize; ++col) {
+            if (canPlace(row, col, solution)) {
+                solution[row] = col;
+                num += placeQueensNum(row + 1, solution, gridSize);
+            }
+        }
+        return num;
+    }
 
-		if (target[row][col+1] == 0)
-		innerCount += countPaths(target, n-1, row, col+1);
+    private static boolean canPlace(int row, int col, int[] solution) {
+        for (int prevRow = 0; prevRow < row; ++prevRow) {
+            if (col == solution[prevRow]) return false;
+            if (row - prevRow == Math.abs(col - solution[prevRow])) return false;
+        }
+        return true;
+    }
 
-		if (target[row+1][col] == 0)
-		innerCount += countPaths(target, n-1, row+1, col);
+    //TEST----------------------------------
+    public static void main(String[] args) {
+        System.out.println("Solutions:");
+        printSolution(placeQueens(8));
+        System.out.println("\nNumber of solutions:"); 
+        System.out.println(placeQueensNum(8)); 
+    }
 
-		return innerCount;
-	}
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int[][] grid = new int[3][3];
-		int pathNum = countPaths(grid, 3, 0, 0);
-		System.out.print(pathNum);
-
-	}
-
+    private static void printSolution(ArrayList<int[]> solutions) {
+        for (int[] solution : solutions) {
+            for (int i : solution) System.out.println(i + " ");
+            System.out.println();
+        }
+    }
 }
